88e9ab56bde224c45e5e1570da24c722f7898ba6
Thomas Luijken <tluijken@outlook.com>
2021-12-05 23:58:19 +0100

    Added part 2 and 'fixed' part one

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,42 +3,40 @@ use std::io::BufReader;
 use std::fs::File;
 
 fn main() {
-    part_1();
-    part_2();
+    let depts: Vec<i32> = BufReader::new(File::open("input").unwrap()).lines()
+        .filter(|line| line.is_ok())
+        .map(|line| line.unwrap().parse::<i32>())
+        .filter(|number| number.is_ok())
+        .map(|item| item.unwrap())
+        .collect();
+
+    part_1(&depts);
+    part_2(&depts);
 }
 
-fn part_1() {
-    let mut prev: Option<i32> = None;
+fn part_1(depts: &[i32]) {
     let mut nr_of_increases = 0; 
-    for(_index, line) in BufReader::new(File::open("input").unwrap()).lines().enumerate(){
-        let curr: i32 = line.unwrap().parse::<i32>().unwrap(); // we assume all is ok for now.
-        if prev.is_some() && prev < Some(curr) {
+ 
+    for x in 1..depts.len() {
+        if depts[x] > depts[x -1] {
             nr_of_increases += 1;
         }
-        
-        prev = Some(curr);
     }
 
     // -1, i.d.k....
     println!("Increases = {}", nr_of_increases - 1);
 }
 
-fn part_2(){
+fn part_2(depts: &[i32]) {
     let mut prev: Option<i32> = None;
     let mut nr_of_increases = 0;
-    let lines: Vec<i32> = BufReader::new(File::open("input").unwrap()).lines()
-        .filter(|line| line.is_ok())
-        .map(|line| line.unwrap().parse::<i32>().unwrap())
-        .collect();
     
-    for x in 0..lines.len() - 2 {
-        let curr: i32 = lines[x] + lines[x + 1] + lines[x+2];
-
-        if prev.is_some() && prev < Some(curr) {
+    for x in 0..depts.len() - 2 {
+        let curr = Some(depts[x] + depts[x+ 1] + depts[x+2]);
+        if prev.is_some() && prev < curr {
             nr_of_increases += 1;
         }
-        
-        prev = Some(curr);
+        prev = curr;
     }
 
     println!("Increases = {}", nr_of_increases);
