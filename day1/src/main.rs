846f53df801596e8f7383f1b85a693966b9b7932
Thomas Luijken <tluijken@outlook.com>
2021-12-05 23:37:24 +0100

    Added part 2 and 'fixed' part one

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,16 +3,43 @@ use std::io::BufReader;
 use std::fs::File;
 
 fn main() {
+    part_1();
+    part_2();
+}
+
+fn part_1() {
     let mut prev: Option<i32> = None;
     let mut nr_of_increases = 0; 
     for(_index, line) in BufReader::new(File::open("input").unwrap()).lines().enumerate(){
         let curr: i32 = line.unwrap().parse::<i32>().unwrap(); // we assume all is ok for now.
-        if prev.is_some() && prev <= Some(curr) {
+        if prev.is_some() && prev < Some(curr) {
             nr_of_increases += 1;
         }
         
         prev = Some(curr);
     }
 
-    println!("Total number of increases is: {}", nr_of_increases);
+    // -1, i.d.k....
+    println!("Increases = {}", nr_of_increases - 1);
+}
+
+fn part_2(){
+    let mut prev: Option<i32> = None;
+    let mut nr_of_increases = 0;
+    let lines: Vec<i32> = BufReader::new(File::open("input").unwrap()).lines()
+        .filter(|line| line.is_ok())
+        .map(|line| line.unwrap().parse::<i32>().unwrap())
+        .collect();
+    
+    for x in 0..lines.len() - 2 {
+        let curr: i32 = lines[x] + lines[x + 1] + lines[x+2];
+
+        if prev.is_some() && prev < Some(curr) {
+            nr_of_increases += 1;
+        }
+        
+        prev = Some(curr);
+    }
+
+    println!("Increases = {}", nr_of_increases);
 }
