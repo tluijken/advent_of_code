0e6342b79a5be89920916be9f04da56164e02fdf
Thomas Luijken <tluijken@outlook.com>
2021-12-08 01:15:31 +0100

    Added day 4 part 1

---
+++ day4/src/main.rs
@@ -0,0 +1,89 @@
+#![feature(entry_insert)]
+
+use std::collections::HashMap;
+fn main() {
+    let lines = include_str!("../input").lines().collect::<Vec<_>>();
+
+    let numbers_drawn = lines.first()
+                             .unwrap()
+                             .split(',')
+                             .map(|s| s.parse::<u32>().unwrap())
+                             .collect::<Vec<_>>();
+
+    let count = lines.len();
+
+    // Construct the boards
+    let mut boards: Vec<Board> = Vec::new();
+    for x in (2..count).step_by(6) {
+        let mut numbers: HashMap<u32, Vec<(u32, bool)>> = HashMap::new();
+        for y in 0..5{
+            let values = lines[x+y].split(' ').filter(|n| !n.is_empty()).map(|l| (l.parse::<u32>().unwrap(), false)).collect();
+            numbers.entry(y as u32).insert(values);
+        }
+        boards.push(Board{
+            numbers
+        });
+    }
+
+
+    let mut winner = false;
+    
+    let mut last_drawn_number: u32 = 0;
+    let mut winner_score: u32 = 0;
+    for number in numbers_drawn {
+        for board in boards.iter_mut() {
+            board.mark_number(&number);
+            if board.check_winner() {
+                winner_score = board.sum_unmarked();
+                winner = true;
+                break;
+            }
+        }
+        if winner {
+            last_drawn_number = number;
+            break;
+        }
+    }
+    
+    println!("Result for step 1 : {}", winner_score * last_drawn_number);
+}
+
+struct Board {
+    numbers: HashMap<u32, Vec<(u32, bool)>>,
+}
+
+
+impl Board{
+    fn check_winner(&self) -> bool {
+        // Check horizontal
+        for values in self.numbers.values() {
+            if values.iter().all(|v| v.1) {
+                return true;
+            }
+        }
+
+        // check vertical
+        for x in 0..5 {
+            if self.numbers.iter().all(|(_, values)| values[x].1) {
+                return true;
+            }
+        }
+        false
+    }
+
+    fn mark_number(&mut self, drawn_number: &u32) {
+        for (_, value) in self.numbers.iter_mut()
+        {
+            for mut number in value {
+                if number.0 == *drawn_number {
+                    number.1 = true;
+                }
+            }
+        }
+    }
+
+    fn sum_unmarked(&self) -> u32 {
+        self.numbers.iter().map(|(_, values)| values.iter().filter(|v| !v.1).map(|v| v.0).sum::<u32>()).sum::<u32>()
+    }
+}
+
