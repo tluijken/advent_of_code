d3eb38546245a6e77a67970ea48682866d6f1266
Thomas Luijken <tluijken@outlook.com>
2021-12-13 22:08:44 +0100

    Day 9 part 2 (refactored part one)

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,5 +1,7 @@
 use crate::input::Input;
 
+const NEIGHBORS: [(isize, isize); 4] = [(0, -1), (0, 1), (-1, 0), (1, 0)];
+
 fn main() {
     const INPUT: &str = include_str!("../input");
     
@@ -14,32 +16,42 @@ fn main() {
 
 
 mod input {
+    use crate::NEIGHBORS;
+
     pub struct Input{
         heightmap: Vec<Vec<u32>>,
     }
 
     impl Input {
         pub fn problem1(& mut self) -> u32 {
-            let length = self.heightmap[0].len();
             let mut result = 0;
-            for y in 0..self.heightmap.len() {
-                for x in 0..length {
-                    let left_greater = x == 0 || self.heightmap[y][x - 1] > self.heightmap[y][x];
-                    let right_greater = x == length - 1 || self.heightmap[y][x + 1] > self.heightmap[y][x];
-                    let top_greater = y == 0 || self.heightmap[y -1][x] > self.heightmap[y][x];
-                    let bottom_greater = y == (self.heightmap.len() - 1) || self.heightmap[y + 1][x] > self.heightmap[y][x];
-                    if left_greater && right_greater && top_greater && bottom_greater {
-                        result += 1 + self.heightmap[y][x];
-                    }
+            for (y, line) in self.heightmap.iter().enumerate() {
+                for (x, cell) in line.iter().enumerate() {
+                    if NEIGHBORS.iter().all(|&(nx, ny)| {
+                        self.heightmap.get((y as isize + ny) as usize)
+                            .and_then(|l| l.get((x as isize + nx) as usize))
+                            .map(|n| cell < n)
+                            .unwrap_or(true)
+                    }) { result += cell + 1; }
                 }
             }
             result
         }
 
+        pub fn problem2(&mut self) -> usize {
+            let mut basins: Vec<usize> = vec![];
+            for y in 0..self.heightmap.len() {
+                for x in 0..self.heightmap.len() {
+                    if self.heightmap[y][x] < 9 {
+                        basins.push(get_basin(&mut self.heightmap, x, y));
+                    }
+                }
+            }
 
-        pub fn problem2(& mut self) -> i32 {
-            0
+            basins.sort_unstable();
+            basins.iter().rev().take(3).product()
         }
+
 
         pub fn from_str(input: &str) -> Self {
 
@@ -49,7 +61,24 @@ mod input {
             }
         }
     }
+
+    /// Scans all NEIGHBORS for x and y resursively and marks handled items for this basin with the
+    /// high-number to it doesn't get used again
+    /// Returns the count of all cells within the basin
+    fn get_basin(heightmap: &mut Vec<Vec<u32>>, x: usize, y: usize) -> usize {
+        heightmap[y][x]= 9; // mark as used
+        NEIGHBORS.iter().map(|(nx, ny)| ((x as isize + nx) as usize, (y as isize + ny) as usize))
+            .fold(1, |acc, (x, y)| {
+                match heightmap.get(y).and_then(|l| l.get(x)).map(|&n| n < 9){
+                    Some(true) => acc + get_basin(heightmap, x, y),
+                    _ => acc
+                }
+            })
+    }
+
 }
+
+
 
 #[cfg(test)]
 mod test {
@@ -71,7 +100,7 @@ mod test {
     #[test]
     fn problem2() {
         let mut input = Input::from_str(INPUT);
-        let expected = 0;
+        let expected = 1134;
         let actual = input.problem2();
         assert_eq!(expected, actual);
     }
