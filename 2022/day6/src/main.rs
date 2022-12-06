b0b4ae03c82ed5ffe6f6226c87ccceadc8c3dc0c
Thomas Luijken <tluijken@outlook.com>
2022-12-06 21:44:42 +0100

    Day6

---
+++ 2022/day6/src/main.rs
@@ -0,0 +1,46 @@
+fn part_1(input: &str) -> usize {
+    get_distinct_sequence(input, 4)
+}
+
+fn part_2(input: &str) -> usize {
+    get_distinct_sequence(input, 14)
+}
+
+fn get_distinct_sequence(input: &str, window_size: usize) -> usize {
+    input
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(window_size)
+        .enumerate()
+        .find(|(_, w)| !w.iter().any(|c| w.iter().filter(|cc| cc == &c).count() > 1))
+        .map(|(i, _)| i)
+        .unwrap()
+        + window_size
+}
+
+fn main() {
+    let input = std::fs::read_to_string("input").unwrap();
+    println!("{}", part_1(&input));
+    println!("{}", part_2(&input));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_part_1() {
+        assert_eq!(7, part_1("mjqjpqmgbljsphdztnvjfqwrcgsmlb"));
+        assert_eq!(5, part_1("bvwbjplbgvbhsrlpgdmjqwftvncz"));
+        assert_eq!(6, part_1("nppdvjthqldpwncqszvftbrmjlhg"));
+        assert_eq!(10, part_1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
+        assert_eq!(11, part_1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
+    }
+    #[test]
+    fn test_part_2() {
+        assert_eq!(19, part_2("mjqjpqmgbljsphdztnvjfqwrcgsmlb"));
+        assert_eq!(23, part_2("bvwbjplbgvbhsrlpgdmjqwftvncz"));
+        assert_eq!(23, part_2("nppdvjthqldpwncqszvftbrmjlhg"));
+        assert_eq!(29, part_2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
+        assert_eq!(26, part_2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
+    }
+}
