bd758d024774afdda4287299a197f094baeae798
Thomas Luijken <tluijken@outlook.com>
2022-12-08 23:11:32 +0100

    Day 8 part 2

--- 2022/day8/src/main.rs
+++ 2022/day8/src/main.rs
@@ -1,3 +1,5 @@
+use Result::{Err as Break, Ok as Next};
+
 fn part_1(input: &str) -> usize {
     let mut treemap = read_tree_map(input);
     check_visible_trees(&mut treemap);
@@ -7,18 +9,73 @@ fn part_1(input: &str) -> usize {
     seen_trees
 }
 
-fn read_tree_map(input: &str) -> Vec<Vec<(u32, bool)>> {
+fn read_tree_map(input: &str) -> Vec<Vec<(u32, bool, u32)>> {
     input.lines().fold(vec![], |mut acc, l| {
         acc.push(
             l.chars()
-                .map(|c| (c.to_digit(10).unwrap(), false))
-                .collect::<Vec<(u32, bool)>>(),
+                .map(|c| (c.to_digit(10).unwrap(), false, 0))
+                .collect::<Vec<(u32, bool, u32)>>(),
         );
         acc
     })
 }
 
-fn check_visible_trees(treemap: &mut Vec<Vec<(u32, bool)>>) {
+fn calc_scenic_score(treemap: &mut Vec<Vec<(u32, bool, u32)>>) {
+    let copy = treemap.clone();
+    treemap.iter_mut().enumerate().for_each(|(i, r)| {
+        r.iter_mut().enumerate().for_each(|(j, c)| {
+            let mut top = 0;
+            let _ = (0..i)
+                .rev()
+                .try_fold(false, |rust_me_daddy, p| {
+                    top = top + 1;
+                    match copy[p][j].0 < c.0 {
+                        true => Next(rust_me_daddy),
+                        false => Break(rust_me_daddy),
+                    }
+                })
+                .unwrap_or_default();
+            let mut bottom = 0;
+            _ = (i..copy.len())
+                .skip(1)
+                .try_fold(false, |i_have_no_idea_what_i_am_doing, p| {
+                    bottom = bottom + 1;
+                    match copy[p][j].0 < c.0 {
+                        true => Next(i_have_no_idea_what_i_am_doing),
+                        false => Break(i_have_no_idea_what_i_am_doing),
+                    }
+                })
+                .unwrap_or_default();
+
+            let mut left = 0;
+            let _ = (0..j)
+                .rev()
+                .try_fold(false, |whatever_gets_the_job_done, p| {
+                    left = left + 1;
+                    match copy[i][p].0 < c.0 {
+                        true => Next(whatever_gets_the_job_done),
+                        false => Break(whatever_gets_the_job_done),
+                    }
+                })
+                .unwrap_or_default();
+
+            let mut right = 0;
+            let _ = (j..copy[0].len())
+                .skip(1)
+                .try_fold(false, |rust_evengalism_strike_force_incoming, p| {
+                    right = right + 1;
+                    match copy[i][p].0 < c.0 {
+                        true => Next(rust_evengalism_strike_force_incoming),
+                        false => Break(rust_evengalism_strike_force_incoming),
+                    }
+                })
+                .unwrap_or_default();
+            c.2 = top * bottom * left * right;
+        });
+    });
+}
+
+fn check_visible_trees(treemap: &mut Vec<Vec<(u32, bool, u32)>>) {
     treemap.iter_mut().for_each(|r| {
         r.iter_mut().fold(None, |mut max, c| {
             if max.is_none() || max.unwrap_or(0) < c.0 {
@@ -54,7 +111,15 @@ fn check_visible_trees(treemap: &mut Vec<Vec<(u32, bool)>>) {
 }
 
 fn part_2(input: &str) -> u32 {
-    todo!();
+    let mut treemap = read_tree_map(input);
+    calc_scenic_score(&mut treemap);
+
+    let highest_scenic_score = treemap
+        .iter()
+        .map(|r| r.iter().map(|c| c.2).max().unwrap_or(0))
+        .max()
+        .unwrap_or(0);
+    highest_scenic_score
 }
 
 fn main() {
@@ -77,6 +142,6 @@ mod tests {
     }
     #[test]
     fn test_part_2() {
-        assert_eq!(45000, part_2(INPUT));
+        assert_eq!(8, part_2(INPUT));
     }
 }
