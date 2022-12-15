5265b9e9f5a632889b8cc3cb91a494d530d89512
Thomas Luijken <tluijken@outlook.com>
2022-12-15 23:29:24 +0100

    Code cleanup day 1

--- 2022/cpp/days/01.cpp
+++ 2022/cpp/days/01.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main() {
   string s;
   ifstream dataFile("../../rust/input/2022/day1.txt");
-  std::vector<int> v;
+  vector<int> v;
   int totalGroupVal = 0;
   while (getline(dataFile, s)) {
     if (s.empty() || !s.find_first_not_of("0123456789")) {
@@ -20,8 +20,8 @@ int main() {
     }
   }
   sort(v.begin(), v.end(), greater<int>());
-  std::cout << "Day 1 part one : " << v[0] << "\n";
-  std::cout << "Day 1 part two : " << v[0] + v[1] + v[2] << "\n";
+  cout << "Day 1 part one : " << v[0] << "\n";
+  cout << "Day 1 part two : " << v[0] + v[1] + v[2] << "\n";
 
   return 0;
 }
