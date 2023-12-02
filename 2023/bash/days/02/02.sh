5d2224d74f2b077b5f7635763e2fb66a176af0d9
Thomas Luijken <thomas@luijken.dev>
2023-12-02 15:08:14 +0100

    prettyfied day 2

--- 2023/bash/days/02/02.sh
+++ 2023/bash/days/02/02.sh
@@ -2,15 +2,14 @@
 #
 gameIdTotals=0
 powers=0
-declare -A thresholds
-thresholds=([red]=12 [blue]=14 [green]=13)
+declare -A thresholds=([red]=12 [blue]=14 [green]=13)
 while read -r line; do
-    gameId=( $(echo "$line" | grep -oP '[0-9]+') )
-    valid=true
-    game=$(echo "$line" | sed "s/Game ${gameId[0]}: //g")
 
-    declare -A cubes
-    cubes=([red]=0 [blue]=0 [green]=0)
+    gameId=$(grep -oP 'Game \K[0-9]+' <<< "$line")
+    valid=true
+    game=$(sed "s/Game $gameId: //g" <<< "$line")
+
+    declare -A cubes=([red]=0 [blue]=0 [green]=0)
 
     IFS=';' read -ra HANDS <<< "$game"
     for i in "${HANDS[@]}"; do
@@ -18,19 +17,12 @@ while read -r line; do
         for j in "${blocks[@]}"; do
             color=$(echo "$j" | grep -oP '[a-z]+')
             number=$(echo "$j" | grep -oP '[0-9]+')
-            if [[ ${thresholds[$color]} -lt $number ]]; then
-                valid=false
-            fi
-            if [[ ${cubes[$color]} -lt $number ]]; then
-                cubes[$color]=$number
-            fi
+            (( ${thresholds[$color]} < number )) && valid=false
+            (( ${cubes[$color]} < number )) && cubes[$color]=$number
         done
     done
-    if [[ $valid == true ]]; then
-        gameIdTotals=$((gameIdTotals + ${gameId[0]}))
-    fi
-    power=$((cubes[red] * cubes[blue] * cubes[green]))
-    powers=$((powers + power))
+    [[ $valid == true ]] && gameIdTotals=$((gameIdTotals + ${gameId[0]}))
+    powers=$((powers + $((cubes[red] * cubes[blue] * cubes[green]))))
 done < input
-echo "$gameIdTotals"
-echo "$powers"
+echo "Part 1: $gameIdTotals"
+echo "Part 2: $powers"
