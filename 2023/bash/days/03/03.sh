b7619c098745eb895e85d49b0a3d5433bf4281b1
Thomas Luijken <thomas@luijken.dev>
2023-12-06 21:56:49 +0100

    optimized day 3 code

--- 2023/bash/days/03/03.sh
+++ 2023/bash/days/03/03.sh
@@ -1,41 +1,24 @@
 #!/bin/bash
-declare -A matrix
-declare -A gears
-l=0;
-c=0;
-sum=0
-gearsTotal=0
-symbol_regex='[^.0-9]'
+declare -A m; declare -A g
+l=0;c=0;sum=0;gearsTotal=0;symbols='[^.0-9]';asterix='[*]'
+
 positions=(-1,-1 -1,0 -1,1 0,-1 0,1 1,-1 1,0 1,1)
 
 while read -r line; do
     for (( i=0; i<${#line}; i++ )); do
-        char=${line:$i:1}
-        matrix[$l,$i]=$char
+        m[$l,$i]=${line:$i:1}
         [[ $i -gt $c ]] && c=$i
     done
     ((l++))
-done < input
+done < $1
 
-is_adjacent_to_symbol() {
-    local i=$1
-    local j=$2
-    for (( k=0; k<${#positions[@]}; k++ )); do
-        pos=(${positions[$k]})
-        if [[ ${matrix[$((i+pos[0])),$((j+pos[1]))]} =~ $symbol_regex ]]; then
-            return 0
-        fi
-    done
-    return 1
-}
+check_adjacent() {
+    local i=$1;local j=$2;local pattern=$3
 
-is_adjacent_to_asterix() {
-    local i=$1
-    local j=$2
     for (( k=0; k<${#positions[@]}; k++ )); do
         IFS=',' read -ra pos <<< "${positions[$k]}"
-        if [[ ${matrix[$((i+pos[0])),$((j+pos[1]))]} == "*" ]]; then
-            echo "$((i+pos[0])),$((j+pos[1]))"
+        if [[ ${m[$((i+pos[0])),$((j+pos[1]))]} =~ $pattern ]]; then
+            echo $((i+pos[0])),$((j+pos[1]))
             return 0
         fi
     done
@@ -44,28 +27,22 @@ is_adjacent_to_asterix() {
 
 for (( i=0; i<l; i++ )); do
     for (( j=0; j<c; j++ )); do
-        if [[ ${matrix[$i,$j]} =~ ^[0-9]+$ ]]; then
-            adjentToSymbol=false
-            num=${matrix[$i,$j]}
-            is_adjacent_to_symbol "$i" "$j" && adjentToSymbol=true
-            gearindex=$(is_adjacent_to_asterix "$i" "$j")
-            while [[ ${matrix[$i,$((j+1))]} =~ ^[0-9]+$ ]]; do
-                num=$num${matrix[$i,$((j+1))]}
+        if [[ ${m[$i,$j]} =~ ^[0-9]+$ ]]; then
+            num=${m[$i,$j]}
+            si=$(check_adjacent "$i" "$j" "$symbols")
+            gi=$(check_adjacent "$i" "$j" "$asterix")
+            while [[ ${m[$i,$((j+1))]} =~ ^[0-9]+$ ]]; do
+                num=$num${m[$i,$((j+1))]}
                 ((j++))
-                is_adjacent_to_symbol "$i" "$j" && adjentToSymbol=true
-                # if the gear index is not set, set it
-                if [[ $gearindex == 0 ]]; then
-                    gearindex=$(is_adjacent_to_asterix "$i" "$j")
-                fi
+                [[ $si == 0 ]] && si=$(check_adjacent "$i" "$j" "$symbols")
+                [[ $gi == 0 ]] && gi=$(check_adjacent "$i" "$j" "$asterix")
             done
-            [[ $adjentToSymbol == true ]] && ((sum+=$num))
-            if [[ $gearindex != 0 ]]; then
-                if [[ ${gears[$gearindex]} == "" ]]; then
-                    # keep track of this gear index and the number, for later reference if we find this same asterix index again
-                    gears[$gearindex]=$num
-                else
-                    ((gearsTotal+=${gears[$gearindex]} * num))
-                fi
+            [[ $si != 0 ]] && ((sum+=$num))
+            [[ $gi == 0 ]] && continue
+            if [[ ${g[$gi]} == "" ]]; then
+                g[$gi]=$num
+            else
+                ((gearsTotal+=${g[$gi]} * num))
             fi
         fi
     done
