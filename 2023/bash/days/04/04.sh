2349b15b7205adb350ce5de319aa696243edf165
Thomas Luijken <thomas@luijken.dev>
2023-12-07 00:45:17 +0100

    Shorthands for day 4

--- 2023/bash/days/04/04.sh
+++ 2023/bash/days/04/04.sh
@@ -1,33 +1,21 @@
 #!/bin/bash
 sum=0
 declare -A m
-originals=0;
+cid=0;
 while read -r line; do
-    ((originals++))
-    cid=$originals
-    # get the number of copies of this card out of the m array
-    copies=${m[$cid]}
-    [[ $copies == "" ]] && copies=0
+    ((cid++))
+    copies=${m[$cid]:-0}
     n=$(sed "s/Card $cid: //g" <<< "$line")
     IFS='|' read -ra nsplit <<< "$n"
     IFS=' ' read -ra wn <<< "${nsplit[0]}"
     IFS=' ' read -ra mn <<< "${nsplit[1]}"
-    matchcount=0
-    for i in "${wn[@]}"; do
-        for j in "${mn[@]}"; do
-            [[ $i == $j ]] && ((matchcount++))
-        done
-    done
+    intersection=($(comm -12 <(printf '%s\n' "${wn[@]}" | sort -u) <(printf '%s\n' "${mn[@]}" | sort -u)))
+    matchcount=${#intersection[@]}  
     [[ $matchcount == 0 ]] && continue
     sum=$((sum + (2 ** (matchcount - 1))))
-    for ((i = cid + 1; i <= matchcount + cid; i++)); do
-        m[$i]=$((m[$i] + 1 + copies))
-    done
+    for i in $(seq $((cid + 1)) $((matchcount + cid))); do ((m[$i] += 1 + copies)); done
 done < $1
 echo $sum
 
-decksum=$originals
-for i in "${!m[@]}"; do
-    decksum=$((decksum + m[$i]))
-done
-echo $decksum
+for i in "${!m[@]}"; do cid=$((cid + m[$i]));done
+echo $cid
