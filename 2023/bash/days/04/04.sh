f07f88e04e4866f95ab38bd7b8469bf4208cbe8b
Thomas Luijken <thomas@luijken.dev>
2023-12-06 23:55:54 +0100

    Day 4 part 2

--- 2023/bash/days/04/04.sh
+++ 2023/bash/days/04/04.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 sum=0
+declare -A m
+originals=0;
 while read -r line; do
-    cid=$(grep -oP 'Card \K[0-9]+' <<< "$line")
+    ((originals++))
+    cid=$originals
+    # get the number of copies of this card out of the m array
+    copies=${m[$cid]}
+    [[ $copies == "" ]] && copies=0
     n=$(sed "s/Card $cid: //g" <<< "$line")
     IFS='|' read -ra nsplit <<< "$n"
     IFS=' ' read -ra wn <<< "${nsplit[0]}"
     IFS=' ' read -ra mn <<< "${nsplit[1]}"
-    score=0
-    for i in "${wn[@]}"; do
-        # check if word is in mn
-        for j in "${mn[@]}"; do
-            if [[ $i == $j ]]; then
-                if [[ $score == 0 ]]; then
-                    score=1
-                else
-                    score=$((score * 2))
-                fi
-            fi
+    for ((c=0; c <= $copies; c++)); do
+        matchCount=0
+        for i in "${wn[@]}"; do
+            for j in "${mn[@]}"; do
+                [[ $i == $j ]] && ((matchCount++))
+            done
+        done
+        [[ $matchCount == 0 ]] && continue
+        [[ $c == 0 ]] && sum=$((sum + (2 ** (matchCount - 1))))
+        for ((i = cid + 1; i <= matchCount + cid; i++)); do
+            m[$i]=$((m[$i] + 1))
         done
     done
-    sum=$((sum + score))
 done < $1
 echo $sum
+
+decksum=$originals
+for i in "${!m[@]}"; do
+    decksum=$((decksum + m[$i]))
+done
+echo $decksum
